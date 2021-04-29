6cc334bba185a789971f78921168aa7f596df712
Chris Wininger <cwininger@sonatype.com>
2021-04-29 12:28:49 -0400

    react-helmet added
    We now dynamically set the title using helmet, but as you'll see this
    code only has an impact client side

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 export default function App() {
   return (
     <main>
+      <Helmet>
+        <title>Much Dynamic, Much Server</title>
+      </Helmet>
       <div>
         Welcome to RSC
       </div>
